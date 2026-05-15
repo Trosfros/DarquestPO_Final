44c156aa9e4ce02225b6e213a56ff15a1516e343
tp <theotime.perras@gmail.com>
2026-05-15 09:35:32 -0400

    url fix (again)

--- reset_password.php
+++ reset_password.php
@@ -42,7 +42,7 @@
               <table border="0" cellpadding="0" cellspacing="0">
                 <tr>
                   <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                    <a href="<?= trim($domain) ?>/Darquest/reset_password_form.php?guid=<?= trim($guid) ?>" 
+                    <a href="<?= $domain . "/reset_password_form.php?guid=" . $guid?>" 
                        target="_blank" 
                        style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px; font-weight: bold; border: 1px solid #1a82e2;">
                        Confirmer le nouveau mot de passe
